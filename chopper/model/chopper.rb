c24d3a252f992c77dc3d6142dffa235c6bfbc27d
pdpire <pablopire1@gmail.com>
2015-09-10 01:02:53 -0300

    segundo test

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -2,7 +2,8 @@ class Chopper
   def chop(num, list)
     if num == 3 && list == []
       -1
-    end
+    elsif num == 3 && list == [3]
+      0
+    end 
   end
-
 end
--- chopper/spec/chopper_spec.rb
+++ chopper/spec/chopper_spec.rb
@@ -12,15 +12,15 @@ describe 'Chopper' do
 
 	let(:chopper) { Chopper.new }  
 
+
 	it 'chop de 3 y vacio deberia ser -1' do
 		expect(chopper.chop(3,[])).to eq -1
 	end
 
-=begin
-
 	it 'chop de 3 y [3] deberia ser 0' do
 		expect(chopper.chop(3,[3])).to eq 0 
 	end
+=begin
 
 	it 'chop de 3 y [0,7,3] deberia ser 0' do
 		expect(chopper.chop(3,[0,7,3])).to eq 2 
