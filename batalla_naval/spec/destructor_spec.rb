3f1670c2d4a33cc9600861a4cb7d4823fbb123c2
pdpire <pablopire1@gmail.com>
2015-10-16 00:39:53 -0300

    segundo test destructor

--- batalla_naval/spec/destructor_spec.rb
+++ batalla_naval/spec/destructor_spec.rb
@@ -6,7 +6,7 @@ describe 'Destructor' do
 
   let(:destructor) { Destructor.new }	
     
-    it 'deberia poner destructor desde pos 4,3 en horizontal' do
+    it 'deberia poner destructor desde pos 2,3 en horizontal' do
 	  @tablero = Tablero.new(6,6)
 	  @sub = Destructor.new
 	  @sub.set_id(30)
@@ -15,5 +15,16 @@ describe 'Destructor' do
 	  expect(@tablero.posicion(2,3)).to eq 30
 	  expect(@tablero.posicion(3,3)).to eq 30
 	  expect(@tablero.posicion(4,3)).to eq 30
+    end
+
+    it 'deberia poner destructor desde pos 2,3 en vertical' do
+	  @tablero = Tablero.new(6,6)
+	  @sub = Destructor.new
+	  @sub.set_id(30)
+	  @sub.poner_vertical(2, 3, @tablero)
+
+	  expect(@tablero.posicion(2,3)).to eq 30
+	  expect(@tablero.posicion(2,4)).to eq 30
+	  expect(@tablero.posicion(2,5)).to eq 30
     end
 end
