926436d0517c7a19bd3c05dfea639ea259fd897f
pdpire <pablopire1@gmail.com>
2015-10-16 00:36:15 -0300

    primer test destructor

---
+++ batalla_naval/model/destructor.rb
@@ -0,0 +1,24 @@
+class Destructor
+  
+  @id
+
+  def set_id(n)
+  	@id = n
+  end
+
+  def get_id
+  	return @id
+  end
+
+  def poner_horizontal(x, y, tablero)
+  	tablero.poner_en_celda(x,y, get_id)
+  	tablero.poner_en_celda(x+1,y, get_id)
+    tablero.poner_en_celda(x+2,y, get_id)
+  end
+
+  def poner_vertical(x, y, tablero)
+  	tablero.poner_en_celda(x,y, get_id)
+  	tablero.poner_en_celda(x,y+1, get_id)
+    tablero.poner_en_celda(x,y+2, get_id)
+  end
+end
---
+++ batalla_naval/spec/destructor_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require_relative '../model/destructor'
+require_relative '../model/tablero'
+
+describe 'Destructor' do
+
+  let(:destructor) { Destructor.new }	
+    
+    it 'deberia poner destructor desde pos 4,3 en horizontal' do
+	  @tablero = Tablero.new(6,6)
+	  @sub = Destructor.new
+	  @sub.set_id(30)
+	  @sub.poner_horizontal(2, 3, @tablero)
+
+	  expect(@tablero.posicion(2,3)).to eq 30
+	  expect(@tablero.posicion(3,3)).to eq 30
+	  expect(@tablero.posicion(4,3)).to eq 30
+    end
+end
