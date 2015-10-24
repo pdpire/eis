dd6f20ab8742638624b0e78fd84dceca52c3beaf
pdpire <pablopire1@gmail.com>
2015-10-24 00:24:40 -0300

    pasa escenario con 2 operaciones

--- calculadora/features/memoria.feature
+++ calculadora/features/memoria.feature
@@ -5,11 +5,10 @@ Feature: memoria de cantidad de operaciones
     Given no hice operaciones
     Then la cantidad de operaciones es 0
 
-  @wip
   Scenario: cuando hago operaciones la cantidad de incrementa
     Given hago una suma
     And hago una resta
-    Then la cantidad de operaiones es 2
+    Then la cantidad de operaciones ahora es 2
 
   @wip
   Scenario: cuando reseteo la cantidad de operaciones vuelve a cero
--- calculadora/features/step_definitions/memoria_steps.rb
+++ calculadora/features/step_definitions/memoria_steps.rb
@@ -6,3 +6,21 @@ Then(/^la cantidad de operaciones es (\d+)$/) do |arg1|
   expect(page).to have_content(0)
 end
 
+
+Given(/^hago una suma$/) do
+  visit 'calculadora'
+  fill_in 'num1', with: 5
+  fill_in 'num2', with: 1
+  click_button('+')
+end
+
+Given(/^hago una resta$/) do
+  fill_in 'num1', with: 3
+  fill_in 'num2', with: 2
+  click_button('-')
+end
+
+Then(/^la cantidad de operaciones ahora es (\d+)$/) do |arg1|
+  expect(page).to have_content(2)
+end
+
